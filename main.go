263e492ca42d579f8cf3d55a1a28b23eaf383d24
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-10-10 10:33:55 -0500

    improving lissjous

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 // `color` viene de el import `image/color`, y siempre se usa la última
 // parte en el código
 var palette = []color.Color{
-	color.White,
+	color.Black,
 	color.RGBA{255, 10, 100, 1},
 	color.RGBA{0, 200, 50, 1},
 	color.RGBA{255, 0, 0, 1},
@@ -32,17 +32,19 @@ var cl = len(palette) - 1
 
 func main() {
 	// fmt.Println(cl)
-	//lissajous(os.Stdout)
-	tweetPlease()
+	lissajous(os.Stdout)
+	// tweetPlease()
 }
 
 // Improve this
 func tweetPlease() {
+	// "178848278-zkemS201RssNCnxRo6fF0dka4jvwJmdEn1KLvt4F",
+	//"n64pxfdGmYZZuyOBiETKdNx0sy4BuoC8bAtyYFukPcWn2", "X7k4YMZVwo1EPxA4apRPoEJPS", "6EgrBsM2XhoIGQloajpKjz0lPlDWBv8xGXKRX4wrzJbqldyzHZ"
 	api := anaconda.NewTwitterApiWithCredentials(
 		os.Getenv("TW_ACCESS_TOKEN"),
 		os.Getenv("TW_ACCESS_TOKEN_SECRET"),
 		os.Getenv("TW_CLIENT"),
-		os.Getenv("TW_CLIENT_VAR"))
+		os.Getenv("TW_CLIENT_SECRET"))
 	var buff = new(bytes.Buffer)
 	lissajous(buff)
 	vals := url.Values{}
@@ -55,36 +57,36 @@ func tweetPlease() {
 	}
 	vals.Set("media_ids", strconv.FormatInt(media.MediaID, 10))
 	fmt.Println(media.MediaID)
-	api.PostTweet("Image from GO", vals)
+	api.PostTweet("Lissajous", vals)
 }
 func lissajous(out io.Writer) {
-	args := os.Args[1:]
+	args := os.Args[1:] // ignorar el primer argumento que es el nombre del comando
 	cycles, _ := strconv.ParseFloat(args[0], 64)
 	const ( // las constantes están disponibles en tiempo de compilación, ser números, strings o booleanos
-		res     = 0.0001 // 'sharpnesss'
-		size    = 200    // la imagen medirá lo doble
-		nframes = 128
-		delay   = 8
+		res     = 0.00001 // 'sharpnesss'
+		size    = 250     // la imagen medirá lo doble
+		nframes = 64
+		delay   = 4
 	)
-	m, _ := strconv.ParseFloat(args[1], 64)
+	m, _ := strconv.ParseFloat(args[1], 64) // Mulitplicador de la Frecuencia
 	freq := rand.Float64() * m
 	anim := gif.GIF{LoopCount: nframes} // Creando un GIF
 	phase := 0.0
 	for i := 0; i < nframes; i++ { // Creando cada cuadro de la animación
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1) // Se usará como un plano cartesiano
 		img := image.NewPaletted(rect, palette)
-		var index = uint8(1)
+		var index = uint8(rand.Intn(cl) + 1)
 		var t2 float64
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < cycles*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			t2 += res
 
-			// Changing colorr every cycle
-			if 3.1415-t2 <= 0.1 {
-				index = uint8(rand.Intn(cl) + 1)
-				t2 = 0
-			}
+			// Changing color every cycle
+			// if math.Pi*-t2 <= 0.1 {
+			// 	index = uint8(rand.Intn(cl) + 1)
+			// 	t2 = 0
+			// }
 			// Creating stripes of specific colors across all
 			// the frames
 
@@ -97,9 +99,9 @@ func lissajous(out io.Writer) {
 			// } else {
 			// 	index = 0
 			// }
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), index)
+			img.SetColorIndex(size+int(x*size), size+int(y*size), index)
 		}
-		phase += 0.2
+		phase += math.Pi / 64
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
